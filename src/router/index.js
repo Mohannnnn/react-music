c34695a9bddc87d045f1ae27090b89d9263c1874
Mohan <1063022109@qq.com>
2018-12-27 17:09:53 +0800

    新增router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 /*
  * @Author: wuhan  [https://github.com/Mohannnnn]
  * @Date: 2018-12-27 17:07:02
- * @Last Modified by:   wuhan
- * @Last Modified time: 2018-12-27 17:07:02
+ * @Last Modified by: wuhan
+ * @Last Modified time: 2018-12-27 17:09:38
  */
-
