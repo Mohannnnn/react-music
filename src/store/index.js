d5a4694bad74528fe6dbd4525855818f62237a3d
Mohan <1063022109@qq.com>
2018-12-27 18:16:32 +0800

    新增store

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,16 @@
  * @Author: wuhan  [https://github.com/Mohannnnn]
  * @Date: 2018-12-27 16:56:59
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-12-27 17:01:07
+ * @Last Modified time: 2018-12-27 18:08:57
  * des: 这里是store的入口文件
  */
+import { createStore , applyMiddleware , compose } from 'redux';
+import reducers from './reducers/index.js';
+import thunk from 'redux-thunk';
+
+const store = createStore(
+    reducers,
+    applyMiddleware(thunk)
+);
+
+export default store;
---
+++ src/store/reducers/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: wuhan  [https://github.com/Mohannnnn]
+ * @Date: 2018-12-27 17:02:13
+ * @Last Modified by: wuhan
+ * @Last Modified time: 2018-12-27 18:09:35
+ * des: redux数据计算中心
+ */
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    
+})
