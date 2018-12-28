5dddc3a7ccb65f57fabdb5bce82ccb90e3dc45b5
Mohan <1063022109@qq.com>
2018-12-28 14:05:55 +0800

    新增state

--- src/store/reducers/state.js
+++ src/store/reducers/state.js
@@ -2,8 +2,9 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-12-27 19:10:49 
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-12-27 19:11:18
+ * @Last Modified time: 2018-12-27 19:29:56
  */
+//设置state的初始状态
 const  state = {
     userInfo : '',
 }
