58e402f36d768ba5dde133e203a0786a6d5fa2a7
Mohan <1063022109@qq.com>
2019-01-03 19:30:42 +0800

    app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
+        <div className="app">
           <Routers/>
+        </div>
       </Provider>
     );
   }
