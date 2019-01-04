3a4d5d07d36462ce4193e8b6fd85c227a04f177b
Mohan <1063022109@qq.com>
2019-01-04 11:38:44 +0800

    修改router

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,24 @@
  * @Author: wuhan  [https://github.com/Mohannnnn]
  * @Date: 2018-12-27 17:07:02
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-12-27 19:24:03
+ * @Last Modified time: 2019-01-03 18:48:28
  */
 import React from 'react';
-import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-import Layout from '../pages/Layout';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import Home from '../pages/home/index';
+import PlayMusic from '../pages/PlayMusic/index';
+import SongMenu from '../pages/songMenu/index';
 
 const Routes = () => {
     return (
-        <div className="app">
-            <Router>
-                <Switch>
-                    <Route path='/' exact component={Layout}/>
-                    <Redirect from='*' to='/404' />
-                </Switch>
-            </Router>
-        </div>
+        <Router>
+            <Switch>
+                <Route path='/home'  component={Home}/>
+                <Route path='/playmusic'  component={PlayMusic}/>
+                <Route path='/songmenu'  component={SongMenu}/>
+                <Redirect from='/' exact to='/home' />
+            </Switch>
+        </Router>
     )
 }
 export default Routes;
