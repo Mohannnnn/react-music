bef5276321717f0d0ba7a1ab7fb7bda3718f1a2a
Mohan <1063022109@qq.com>
2019-01-06 21:16:50 +0800

    store

--- src/store/reducers/state.js
+++ src/store/reducers/state.js
@@ -2,10 +2,92 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-12-27 19:10:49 
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-12-27 19:29:56
+ * @Last Modified time: 2019-01-06 20:49:31
  */
 //设置state的初始状态
-const  state = {
-    userInfo : '',
+const state = {
+  userInfo: '',
+  homeRecommend: {
+    "request": false,
+    "data": {
+      "hasTaste": false,
+      "category": 0,
+      "_list": [
+        [{
+          "id": 2553249650,
+          "type": 0,
+          "name": "新的一年，希望你喜欢的人也喜欢你",
+          "copywriter": "编辑推荐：我最喜欢你的地方还是——你喜欢我呀",
+          "picUrl": "http:\u002F\u002Fp1.music.126.net\u002F3t8vLEdBM8ahwJVdt6c3GA==\u002F109951163761633425.jpg",
+          "canDislike": false,
+          "playCount": 6379375.5,
+          "trackCount": 100,
+          "highQuality": false,
+          "alg": "featured"
+        }, {
+          "id": 2499700079,
+          "type": 0,
+          "name": "独立摇滚的引航|照亮航行之路的灯塔",
+          "copywriter": "编辑推荐：坚持自己的观点和见解是独立摇滚的精髓所在",
+          "picUrl": "http:\u002F\u002Fp1.music.126.net\u002FSrIAd7XoJE-E1_c0WKx15w==\u002F109951163729055533.jpg",
+          "canDislike": false,
+          "playCount": 883787.8,
+          "trackCount": 85,
+          "highQuality": false,
+          "alg": "featured"
+        }, {
+          "id": 601208698,
+          "type": 0,
+          "name": "理性的光辉——巴洛克baroque精选集",
+          "copywriter": "根据你喜欢的标签 古典 推荐",
+          "picUrl": "http:\u002F\u002Fp1.music.126.net\u002F_8ZxhJsfjMZjSWms4ls3Pw==\u002F18738976674114520.jpg",
+          "canDislike": true,
+          "playCount": 200195.8,
+          "trackCount": 277,
+          "highQuality": false,
+          "alg": "taste"
+        }],
+        [{
+          "id": 2562517947,
+          "type": 0,
+          "name": "民谣江湖里的路人甲",
+          "copywriter": "热门歌单推荐",
+          "picUrl": "http:\u002F\u002Fp2.music.126.net\u002FHPo0etKU5JLIJ7CN9VHkGw==\u002F109951163733643999.jpg",
+          "canDislike": true,
+          "playCount": 65443,
+          "trackCount": 33,
+          "highQuality": false,
+          "alg": "hot_server"
+        }, {
+          "id": 2509050121,
+          "type": 0,
+          "name": "三角符文 Deltarune 『持续更新』",
+          "copywriter": "热门歌单推荐",
+          "picUrl": "http:\u002F\u002Fp1.music.126.net\u002FIUMMsKmoj-d3F6gCcrVcOg==\u002F109951163660449659.jpg",
+          "canDislike": true,
+          "playCount": 30919,
+          "trackCount": 54,
+          "highQuality": false,
+          "alg": "hot_server"
+        }, {
+          "id": 2554868476,
+          "type": 0,
+          "name": "【说唱伴奏】制作人精选高品质说唱伴奏",
+          "copywriter": "热门歌单推荐",
+          "picUrl": "http:\u002F\u002Fp1.music.126.net\u002FOYSyO-WJCNXDlFoQ4YeY2g==\u002F109951163723261301.jpg",
+          "canDislike": true,
+          "playCount": 8062.6,
+          "trackCount": 28,
+          "highQuality": false,
+          "alg": "hot_server"
+        }]
+      ]
+    },
+    "msg": "",
+    "new_request": true,
+    "new_data": [],
+    "new_msg": "",
+    "loaded": true
+  }
 }
 export default state;
