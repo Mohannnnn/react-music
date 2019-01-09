dbf0602cb747c4461e841d0a3f1452c470343f8f
Mohan <1063022109@qq.com>
2019-01-09 20:02:48 +0800

    获取接口数据

--- src/api/getData.js
+++ src/api/getData.js
@@ -2,7 +2,7 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-19 21:16:14 
  * @Last Modified by: wuhan
- * @Last Modified time: 2018-10-11 10:52:04
+ * @Last Modified time: 2019-01-09 20:02:17
  */
 
 import fetch from './fetch';
@@ -15,71 +15,4 @@ export const getCurLocation = (latitude = 40.032344 , longitude = 116.281948) =>
         latitude : latitude ,
         longitude : longitude
     }
-})
-
-//获取热搜列表
-export const getSearchHotList = (latitude , longitude) => fetch({
-    url : requestUrl.searchHotList,
-    data : {
-        latitude : latitude,
-        longitude : longitude
-    }
-})
-
-//获取搜索推荐结果列表
-export const getSearchRelateList = (key , latitude , longitude , cityId = 3) => fetch({
-    url : requestUrl.searchRelateList,
-    data : {
-        kw : key ,
-        latitude : latitude,
-        longitude : longitude,
-        city_id : cityId
-    }
-})
-
-//首页外卖类型接口列表
-export const getMsiteModeList = (latitude , longitude) => fetch({
-    url : requestUrl.msiteModeList,
-    data : {
-        terminal      : 'h5',
-        latitude      : latitude,
-        longitude     : longitude 
-    }
-})
-
-//首页banner接口
-export const getMsiteBannerList = (latitude , longitude) => fetch({
-    url : requestUrl.msiteBannerList,
-    data : {
-        consumer      : 1,
-        type          : 1,
-        latitude      : latitude,
-        longitude     : longitude 
-    }
-})
-
-//首页推荐商家bar接口
-export const getMsiteBarList = (latitude , longitude) => fetch({
-    url : requestUrl.msiteBarList,
-    data : {
-        terminal      : 'h5',
-        latitude      : latitude,
-        longitude     : longitude 
-    }
-})
-
-//首页商家接口
-export const getRestaurantsList = (latitude , longitude , datas = {}) => fetch({
-    url : requestUrl.restaurantsList,
-    data :  Object.assign({
-            terminal      : 'h5',
-            latitude      : latitude,
-            longitude     : longitude,
-            offset        : 0,
-            limit         : 8,
-            'extras[]'    : 'activities',
-            // 'extras[]'    : 'tags',
-            extra_filters : 'home',
-            rank_id       : ''
-        } ,datas)
 })
