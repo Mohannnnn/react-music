8504d40282d1a0be12b64226008dc4dde7d1669d
Mohan <1063022109@qq.com>
2019-01-11 20:56:08 +0800

    修改request接口

--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -1,8 +1,8 @@
 /*
  * @Author: wuhan  [https://github.com/Mohannnnn] 
- * @Date: 2018-09-19 21:15:47 
+ * @Date: 2018-12-19 21:15:47 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-09 20:02:26
+ * @Last Modified time: 2019-01-11 14:47:21
  */
 
  //请求接口地址配置
