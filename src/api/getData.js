e6af8c2c47e4eb0ce91e5622e1c08381c4aa49ee
Mohan <1063022109@qq.com>
2019-01-16 23:45:21 +0800

    添加接口

--- src/api/getData.js
+++ src/api/getData.js
@@ -2,20 +2,40 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-19 21:16:14 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-14 23:23:31
+ * @Last Modified time: 2019-01-16 10:37:36
  */
 
 import fetch from '../utils/fetch';
 import requestUrl from './requestUrl';
  
 //获取热门歌单列表
-export const getAlbumList = () => fetch({
+export const getAlbumList = ({limit = 12 , order = 'hot'} = {}) => fetch({
     url : requestUrl.albumList,
     data : {
         key : 579621905,
         cat : '全部',
-        limit : 12,
+        limit : limit,
         offset : 0 ,
-        order : 'hot'
+        order : order
+    }
+})
+
+//获取歌曲列表
+export const getSongList = ({limit = 20 , id = 3778678} = {}) => fetch({
+    url : requestUrl.songList,
+    data : {
+        key : 579621905,
+        id : id ,
+        limit : limit,
+        offset : 0
+    }
+})
+
+//获取歌曲信息
+export const getSongMsg = ({id = 516728102} = {}) => fetch({
+    url : requestUrl.songMsg,
+    data : {
+        key: 579621905,
+        id : id
     }
 })
--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -2,17 +2,14 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-12-19 21:15:47 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-14 22:49:49
+ * @Last Modified time: 2019-01-16 10:33:04
  */
 
  //请求接口地址配置
  const requestUrl = {
-    albumList : 'https://api.bzqll.com/music/netease/hotSongList', //热门歌单列表
-    songList : 'https://api.bzqll.com/music/netease/songList',//歌曲列表
-    songMsg : 'https://api.bzqll.com/music/netease/song'//歌曲信息
-//    https://api.bzqll.com/music/netease/hotSongList?key=579621905&cat=%E5%85%A8%E9%83%A8&limit=10&offset=0&order=hot
-//    https://api.bzqll.com/music/netease/songList?key=579621905&id=3778678&limit=10&offset=0
-//    https://api.bzqll.com/music/netease/song?key=579621905&id=516728102
+    albumList : 'https://api.bzqll.com/music/netease/hotSongList',  //热门歌单列表
+    songList : 'https://api.bzqll.com/music/netease/songList',      //歌曲列表
+    songMsg : 'https://api.bzqll.com/music/netease/song',           //歌曲信息
 
 }
 export  default requestUrl;
