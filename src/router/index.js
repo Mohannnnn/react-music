ff28d24ea14d19bfec4363fd4528a8ec670b4c42
Mohan <1063022109@qq.com>
2019-01-25 19:28:37 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,13 @@
  * @Author: wuhan  [https://github.com/Mohannnnn]
  * @Date: 2018-12-27 17:07:02
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-16 11:18:27
+ * @Last Modified time: 2019-01-25 19:25:08
  */
 import React from 'react';
 import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 import Home from '../pages/home/index';
 import PlayMusic from '../pages/PlayMusic/index';
-import SongMenu from '../pages/songMenu/index';
+import AlbumDetail from '../pages/albumDetail/index';
 
 const Routes = () => {
     return (
@@ -16,7 +16,7 @@ const Routes = () => {
             <Switch>
                 <Route path='/home'  component={Home}/>
                 <Route path='/playmusic'  component={PlayMusic}/>
-                <Route path='/songmenu'  component={SongMenu}/>
+                <Route path='/albumdetail'  component={AlbumDetail}/>
                 <Redirect to="/home" />
             </Switch>
         </Router>
