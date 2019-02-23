b8a40a2c2be5fec7ffa5ab3d7822ca5b31296e0d
Mohan <1063022109@qq.com>
2019-02-23 23:15:43 +0800

    actionTypes

--- src/store/actionTypes/index.js
+++ src/store/actionTypes/index.js
@@ -2,8 +2,19 @@
  * @Author: wuhan  [https://github.com/Mohannnnn]
  * @Date: 2018-12-27 17:02:13
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-14 15:06:53
+ * @Last Modified time: 2019-02-22 16:21:50
  * des: actions类型
  */
-export const UPDATE_USERINFO = 'UPDATE_USERINFO';
-export const GET_ALBUMLIST = 'GET_ALBUMLIST';
+//用户信息
+ export const USERINFO_UPDATE = 'USERINFO_UPDATE';
+//首页歌单
+export const ALBUMLIST_UPDATE = 'ALBUMLIST_UPDATE';
+//播放列表
+export const SONGLIST_UPDATE = 'SONGLIST_UPDATE';
+export const SONGLIST_ADD = 'SONGLIST_ADD';
+export const SONGLIST_DELETE = 'SONGLIST_DELETE';
+export const SONGLIST_DELETEALL = 'SONGLIST_DELETEALL';
+//当前播放歌曲操作
+export const SONGPLAYSTATUS_UPDATE= 'SONGPLAYSTATUS_UPDATE';
+export const SONGPLAYTIME_UPDATE = 'SONGPLAYTIME_UPDATE';
+export const SONGPLAYVOLUME_UPDATE = 'SONGPLAYVOLUME_UPDATE';
