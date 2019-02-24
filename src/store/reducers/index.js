262c1e1ec423f70cd5ed7c42891e1052945c101a
Mohan <1063022109@qq.com>
2019-02-24 21:18:37 +0800

    reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,7 +2,7 @@
  * @Author: wuhan  [https://github.com/Mohannnnn]
  * @Date: 2018-12-27 17:02:13
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-19 19:06:55
+ * @Last Modified time: 2019-02-24 17:15:52
  * des: redux数据计算中心
  */
 import { combineReducers } from 'redux';
@@ -11,7 +11,7 @@ import states from './state';
 
 const userInfo = (state = states.userInfo, action) => {
     switch (action.type) {
-        case actionsTypes.UPDATE_USERINFO : 
+        case actionsTypes.USERINFO_UPDATE : 
             return action.data;
         default:
             return state;
@@ -21,15 +21,80 @@ const userInfo = (state = states.userInfo, action) => {
 //推荐歌单
 const albumList = (state = states.albumList , action) =>{
     switch (action.type) {
-        case actionsTypes.GET_ALBUMLIST :
+        case actionsTypes.ALBUMLIST_UPDATE :
             return action.data;
         default:
             return state;
     }
 }
 
+//播放歌单
+const songList = (state = states.songList , action) => {
+    switch (action.type) {
+        case actionsTypes.SONGLIST_ADD :
+            let arr = [...state , action.data];
+            let obj = {};
+            return arr.reduce((cur , next) => {
+                obj[next.id] ? '' : obj[next.id] = true && cur.push(next);
+                return cur;
+            },[]);
+        case actionsTypes.SONGLIST_DELETE :
+            return state.filter(ele => {
+                if(ele.id != action.data.id) return ele;
+            });
+        case actionsTypes.SONGLIST_DELETEALL :
+            return state = [];
+        default:
+            return state; 
+    }
+}
+
+//当前播放歌曲
+const songPlayCur = (state = states.songPlayCur , action) => {
+    switch (action.type) {
+        case actionsTypes.SONGPLAYCUR_UPDATE : 
+            return action.data;
+        default :
+            return state;
+    }
+}
+
+//播放状态
+const songPlayStatus = (state = states.songPlayStatus , action) => {
+    switch (action.type) {
+        case actionsTypes.SONGPLAYSTATUS_UPDATE : 
+            return action.data;
+        default :
+            return state;
+    }
+}
+
+//播放时间
+const songPlayTime = (state = states.songPlayTime , action) => {
+    switch (action.type) {
+        case actionsTypes.SONGPLAYTIME_UPDATE : 
+            return action.data;
+        default :
+            return state;
+    }
+}
+
+//播放音量
+const songPlayVolume = (state = states.songPlayVolume , action) => {
+    switch (action.type) {
+        case actionsTypes.SONGPLAYVOLUME_UPDATE : 
+            return action.data;
+        default :
+            return state;
+    }
+}
 
 export default combineReducers({
     userInfo,
-    albumList
+    albumList,
+    songList,
+    songPlayCur,
+    songPlayStatus,
+    songPlayTime,
+    songPlayVolume
 })
--- src/store/reducers/state.js
+++ src/store/reducers/state.js
@@ -2,11 +2,16 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-12-27 19:10:49 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-14 23:03:59
+ * @Last Modified time: 2019-02-24 17:20:46
  */
 //设置state的初始状态
 const state = {
-  userInfo: '',
-  albumList: []
+  userInfo: '',             //用户信息
+  albumList: [],            //首页播放歌单
+  songList: [],             //播放歌曲列表({id,type}) 
+  songPlayCur : {},         //当前播放歌曲信息
+  songPlayStatus: false,    //播放状态
+  songPlayTime : 0,         //播放时间
+  songPlayVolume: 0.5,      //播放音量
 }
 export default state;
