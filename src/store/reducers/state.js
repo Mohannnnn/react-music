4801e07e19d97fd2f9d1fdce4b7c8446ff50ad96
Mohan <1063022109@qq.com>
2019-02-28 15:27:58 +0800

    修改state

--- src/store/reducers/state.js
+++ src/store/reducers/state.js
@@ -2,13 +2,13 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-12-27 19:10:49 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-02-24 17:20:46
+ * @Last Modified time: 2019-02-28 10:27:32
  */
 //设置state的初始状态
 const state = {
   userInfo: '',             //用户信息
   albumList: [],            //首页播放歌单
-  songList: [],             //播放歌曲列表({id,type}) 
+  songList: [],             //播放歌曲列表({id,type,name,songer,}) 
   songPlayCur : {},         //当前播放歌曲信息
   songPlayStatus: false,    //播放状态
   songPlayTime : 0,         //播放时间
