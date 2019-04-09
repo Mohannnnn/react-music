9fd6f3d30cf6eb7283d7cd0bf081bfd34fdd4683
Mohan <1063022109@qq.com>
2019-04-09 10:48:19 +0800

    修改接口

--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -2,18 +2,18 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-12-19 21:15:47 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-02-19 14:08:47
+ * @Last Modified time: 2019-04-09 10:46:31
  */
 
- //请求接口地址配置
- const requestUrl = {
-    albumList : 'https://api.bzqll.com/music/netease/hotSongList',  //热门歌单列表
-    songList : 'https://api.bzqll.com/music/netease/songList',      //歌单歌曲列表
-    netEaseSearch : 'https://api.bzqll.com/music/netease/search',   //网易云搜索
-    qqSearch : 'https://api.bzqll.com/music/tencent/search',        //QQ音乐搜索
-    kugouSearch : 'https://api.bzqll.com/music/kugou/search',       //酷狗搜索
-    netEaseSongMsg : 'https://api.bzqll.com/music/netease/song',    //网易云歌曲信息
-    qqSongMsg : 'https://api.bzqll.com/music/tencent/song',         //QQ歌曲信息
-    kuGouSongMsg : 'https://api.bzqll.com/music/kugou/song',        //酷狗歌曲信息
+//请求接口地址配置
+const requestUrl = {
+    albumList: 'https://api.itooi.cn/music/netease/hotSongList', //热门歌单列表
+    songList: 'https://api.itooi.cn/music/netease/songList', //歌单歌曲列表
+    netEaseSearch: 'https://api.itooi.cn/music/netease/search', //网易云搜索
+    qqSearch: 'https://api.itooi.cn/music/tencent/search', //QQ音乐搜索
+    kugouSearch: 'https://api.itooi.cn/music/kugou/search', //酷狗搜索
+    netEaseSongMsg: 'https://api.itooi.cn/music/netease/song', //网易云歌曲信息
+    qqSongMsg: 'https://api.itooi.cn/music/tencent/song', //QQ歌曲信息
+    kuGouSongMsg: 'https://api.itooi.cn/music/kugou/song', //酷狗歌曲信息
 }
-export  default requestUrl;
+export default requestUrl;
