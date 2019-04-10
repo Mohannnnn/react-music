a0ef61c77c135b1c7c59c8318dcf3a668661afab
Mohan <1063022109@qq.com>
2019-04-10 13:38:08 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@
  * @Author: wuhan  [https://github.com/Mohannnnn]
  * @Date: 2018-12-27 17:07:02
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-01-27 18:43:56
+ * @Last Modified time: 2019-04-09 15:38:20
  */
 import React from 'react';
-import {HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../pages/home/index';
 import songDetail from '../pages/songDetail/index';
 import AlbumDetail from '../pages/albumDetail/index';
