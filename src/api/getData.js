ead94e0e66a50adafcd81f592d28a6a63286b990
Mohan <1063022109@qq.com>
2019-04-13 19:32:39 +0800

    添加酷我接口

--- src/api/getData.js
+++ src/api/getData.js
@@ -2,61 +2,70 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-09-19 21:16:14 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-02-19 14:06:05
+ * @Last Modified time: 2019-04-12 18:26:28
  */
 
 import fetch from '../utils/fetch';
 import ajax from '../utils/ajax';
 import requestUrl from './requestUrl';
- 
+
 //获取热门歌单列表
-export const getAlbumList = ({limit = 12 , order = 'hot'} = {}) => fetch({
-    url : requestUrl.albumList,
-    data : {
-        key : 579621905,
-        cat : '全部',
-        limit : limit,
-        offset : 0 ,
-        order : order
+export const getAlbumList = ({ limit = 12, order = 'hot' } = {}) => fetch({
+    url: requestUrl.albumList,
+    data: {
+        key: 579621905,
+        cat: '全部',
+        limit: limit,
+        offset: 0,
+        order: order
     }
 })
 
 
 //获取歌曲列表
-export const getSongList = ({limit = 20 , id = 3778678} = {}) => fetch({
-    url : requestUrl.songList,
-    data : {
-        key : 579621905,
-        id : id ,
-        limit : limit,
-        offset : 0
+export const getSongList = ({ limit = 20, id = 3778678 } = {}) => fetch({
+    url: requestUrl.songList,
+    data: {
+        key: 579621905,
+        id: id,
+        limit: limit,
+        offset: 0
     }
 })
 
 //获取网易云歌曲信息
-export const getNetEaseSongMsg = ({id = 516728102} = {}) => fetch({
-    url : requestUrl.netEaseSongMsg,
-    data : {
+export const getNetEaseSongMsg = ({ id = 516728102 } = {}) => fetch({
+    url: requestUrl.netEaseSongMsg,
+    data: {
         key: 579621905,
-        id : id
+        id: id
     }
 })
 
 //获取QQ歌曲信息
-export const getQqSongMsg = ({id = 516728102} = {}) => fetch({
-    url : requestUrl.qqSongMsg,
-    data : {
+export const getQqSongMsg = ({ id = 516728102 } = {}) => fetch({
+    url: requestUrl.qqSongMsg,
+    data: {
         key: 579621905,
-        id : id
+        id: id
     }
 })
 
 //获取酷狗歌曲信息
-export const getKuGouSongMsg = ({id = 516728102} = {}) => fetch({
-    url : requestUrl.kuGouSongMsg,
-    data : {
+export const getKuGouSongMsg = ({ id = 516728102 } = {}) => fetch({
+    url: requestUrl.kuGouSongMsg,
+    data: {
         key: 579621905,
-        id : id
+        id: id
+    }
+})
+
+//获取酷我歌曲信息
+export const getKuWoSongMsg = ({ id = 516728102 } = {}) => fetch({
+    url: requestUrl.kuWoSongMsg,
+    data: {
+        key: 579621905,
+        id: id
     }
 })
 
@@ -64,44 +73,44 @@ export const getKuGouSongMsg = ({id = 516728102} = {}) => fetch({
 // 1. 音乐搜索:type=song
 // 2. 专辑搜索:type=album
 // 3. 歌单搜索:type=list
-export const getNetEaseSearch = ({ s = '', type = 'song' , limit = 10 } = {}) => fetch({
-    url : requestUrl.netEaseSearch,
-    data : {
+export const getNetEaseSearch = ({ s = '', type = 'song', limit = 10 } = {}) => fetch({
+    url: requestUrl.netEaseSearch,
+    data: {
         key: 579621905,
-        s : s,
-        type : type,
-        limit : limit,
-        offset : 0
+        s: s,
+        type: type,
+        limit: limit,
+        offset: 0
     }
-}) 
+})
 
 //QQ音乐歌单搜索
 // 1. 音乐搜索:type=song
 // 2. 专辑搜索:type=album
 // 3. 用户搜索:type=user
-export const getQqSearch = ({ s = '', type = 'song' , limit = 10 } = {}) => fetch({
-    url : requestUrl.qqSearch,
-    data : {
+export const getQqSearch = ({ s = '', type = 'song', limit = 10 } = {}) => fetch({
+    url: requestUrl.qqSearch,
+    data: {
         key: 579621905,
-        s : s,
-        type : type,
-        limit : limit,
-        offset : 0
+        s: s,
+        type: type,
+        limit: limit,
+        offset: 0
     }
-}) 
+})
 
 //酷狗歌单搜索
 // 1. 音乐搜索:type=song
 // 2. 专辑搜索:type=album
 // 3. 用户搜索:type=user
-export const getKugouSearch = ({ s = '', type = 'song' , limit = 10 } = {}) => fetch({
-    url : requestUrl.kugouSearch,
-    data : {
+export const getKugouSearch = ({ s = '', type = 'song', limit = 10 } = {}) => fetch({
+    url: requestUrl.kugouSearch,
+    data: {
         key: 579621905,
-        s : s,
-        type : type,
-        limit : limit,
-        offset : 0
+        s: s,
+        type: type,
+        limit: limit,
+        offset: 0
     }
 })
 
@@ -119,3 +128,18 @@ export const getKugouSearch = ({ s = '', type = 'song' , limit = 10 } = {}) => f
 //         showtype : 1
 //     },data)
 // })
+
+//酷狗歌单搜索
+// 1. 音乐搜索:type=song
+// 2. 专辑搜索:type=album
+// 3. 用户搜索:type=user
+export const getKuwoSearch = ({ s = '', type = 'song', limit = 10 } = {}) => fetch({
+    url: requestUrl.kuWoSearch,
+    data: {
+        key: 579621905,
+        s: s,
+        type: type,
+        limit: limit,
+        offset: 0
+    }
+})
--- src/api/requestUrl.js
+++ src/api/requestUrl.js
@@ -2,7 +2,7 @@
  * @Author: wuhan  [https://github.com/Mohannnnn] 
  * @Date: 2018-12-19 21:15:47 
  * @Last Modified by: wuhan
- * @Last Modified time: 2019-04-09 10:46:31
+ * @Last Modified time: 2019-04-12 18:23:25
  */
 
 //请求接口地址配置
@@ -12,8 +12,10 @@ const requestUrl = {
     netEaseSearch: 'https://api.itooi.cn/music/netease/search', //网易云搜索
     qqSearch: 'https://api.itooi.cn/music/tencent/search', //QQ音乐搜索
     kugouSearch: 'https://api.itooi.cn/music/kugou/search', //酷狗搜索
+    kuWoSearch: 'https://api.itooi.cn/music/kuwo/search', //酷我搜索
     netEaseSongMsg: 'https://api.itooi.cn/music/netease/song', //网易云歌曲信息
     qqSongMsg: 'https://api.itooi.cn/music/tencent/song', //QQ歌曲信息
     kuGouSongMsg: 'https://api.itooi.cn/music/kugou/song', //酷狗歌曲信息
+    kuWoSongMsg: 'https://api.itooi.cn/music/kuwo/song', //酷我歌曲信息
 }
 export default requestUrl;
