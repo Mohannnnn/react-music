17e05d980c3fb13082c783ddd509d897263d0e14
Mohan <1063022109@qq.com>
2019-05-17 19:57:51 +0800

    state

--- src/store/reducers/state.js
+++ src/store/reducers/state.js
@@ -6,13 +6,13 @@
  */
 //设置state的初始状态
 const state = {
-  userInfo: '',             //用户信息
-  albumList: [],            //首页播放歌单
-  songList: [],             //播放歌曲列表({id,type,name,songer,}) 
-  songPlayCur : {},         //当前播放歌曲信息
-  songPlayStatus: false,    //播放状态
-  songPlayTime : 0,         //播放时间
-  songPlayVolume: 0.5,      //播放音量
-  songPlayMode: 0,          //播放模式(0.顺序播放，1.随机播放，其他在这后面加)
+    userInfo: '', //用户信息
+    albumList: [], //首页播放歌单
+    songList: [], //播放歌曲列表({id,type,name,songer,lrc,pic,time,url})
+    songPlayCur: {}, //当前播放歌曲信息
+    songPlayStatus: false, //播放状态
+    songPlayTime: 0, //播放时间
+    songPlayVolume: 0.5, //播放音量
+    songPlayMode: 0, //播放模式(0.顺序播放，1.随机播放，其他在这后面加)
 }
 export default state;
